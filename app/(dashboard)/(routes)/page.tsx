d46c080b685af2eb2391d5024b5ddeec8cf7d573
ArnoldRamodike <riccoarnold23@gmail.com>
2024-06-01 16:20:57 +0200

    Navbar and sidebar full and file folder

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+
+
 
 export default function Home() {
   return (
     <div className="">
-      Protected folder
+      <UserButton afterSignOutUrl="/"/>
     </div>
   );
 }
---
+++ app/(dashboard)/_components/SidebarRoutes.tsx
@@ -0,0 +1,63 @@
+'use client'
+import { BarChart, Compass, Contact2, ContactRound, Layout, List } from 'lucide-react'
+import React from 'react'
+import SidebarItem from './SidebarItem'
+import { usePathname } from 'next/navigation'
+
+const guestRoutes = [
+    {
+        icon: Layout,
+        label: "Dashboard",
+        href: "/"
+    },
+    {
+        icon: ContactRound,
+        label: "Content",
+        href: "/content"
+    },
+    {
+        icon: Compass,
+        label: "Browse",
+        href: "/search"
+    },
+]
+const teacherRoutes = [
+    {
+        icon: List,
+        label: "Courses",
+        href: "/teacher/courses"
+    },
+    {
+        icon: BarChart,
+        label: "Analytics",
+        href: "/teacher/analytics"
+    },
+    {
+        icon: Contact2,
+        label: "Users",
+        href: "/teacher/analytics"
+    },
+]
+
+const SidebarRoutes = () => {
+   const pathname = usePathname();
+
+   const isTeacherPage = pathname?.includes("/teacher");
+
+   const routes = isTeacherPage ? teacherRoutes : guestRoutes;
+
+  return (
+    <div  className='flex flex-col w-full'>
+        {routes.map((route) => (
+            <SidebarItem 
+              key={route.href}
+              icon={route.icon}
+              label={route.label}
+              href={route.href}
+            />
+        ))}
+    </div>
+  )
+}
+
+export default SidebarRoutes
