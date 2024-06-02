601da73750364984c26087a0b3c9776fee377d21
ArnoldRamodike <riccoarnold23@gmail.com>
2024-06-02 23:35:45 +0200

    File upload with upload thing and full functionality

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,36 @@
+import { auth } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+ 
+const f = createUploadthing();
+ 
+// const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+ 
+const handleAuth = () => {
+  const {userId} = auth();
+  console.log(userId);
+  
+
+  if(!userId) throw new Error("Unauthorised");
+
+  return {userId}
+}
+
+export const ourFileRouter = {
+  courseImage: f({image: {maxFileSize: "4MB", maxFileCount: 1}})
+  .middleware(() => handleAuth())
+  .onUploadComplete(() => {}),
+
+  courseAttachment: f(["text", "image", "video", "audio", "pdf" ])
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  
+  chapterVideo: f({video: {maxFileCount: 1, maxFileSize: "1GB"}})
+  .middleware(() => handleAuth())
+  .onUploadComplete(() => {}),
+
+
+
+} satisfies FileRouter;
+ 
+export type OurFileRouter = typeof ourFileRouter;
